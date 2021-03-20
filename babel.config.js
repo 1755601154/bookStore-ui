f7b34ca7a0bcb7caa98db3908da8e18544dc583d
yuanlei <yuanlei@asiainfo.com>
2021-03-20 17:24:16 +0800

    修复编译报错

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  proxy: {
-    '/': {
-      target: 'http://localhost:8060/',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/': '/'
-      }
-    }
-  }
+  ]
 }
